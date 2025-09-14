7e6853a4e0f564255729bc1d3142e7007474f254
huangjoseph45 <huangjoseph45@gmail.com>
2025-09-14 19:33:35 -0400

    basic server

--- JosephHuang/main.go
+++ JosephHuang/main.go
@@ -1,11 +1,29 @@
-package josephhuang
+package main
 
 import (
-	"github.com/go-chi/chi"
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/go-chi/chi/v5"
 )
 
 
 func main(){
-	mux := chi.NewMux()
 	r := chi.NewRouter()
+
+
+	r.Get("/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "hello world")
+	})
+
+
+	addr := ":2000"
+	log.Printf("Server listening on %s\n", addr)
+
+	err := http.ListenAndServe(addr, r)
+	if  err != nil {
+		log.Fatal(err)
+	}
+
 }
