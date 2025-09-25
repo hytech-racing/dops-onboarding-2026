d27e57faa7807f0328199e48df87561ff1be3234
Sophia Nemes <186420664+asnems@users.noreply.github.com>
2025-09-25 16:35:12 -0400

    c1 updated

--- SophiaNemes/main.go
+++ SophiaNemes/main.go
@@ -1,13 +1,29 @@
 package main
 
 import (
-	"fmt"
+	"encoding/json"
 	"net/http"
 	"strings"
 
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
 )
+
+// JSON response structs
+type McapSuccess struct {
+	Message string   `json:"message"`
+	File    McapFile `json:"file"`
+}
+
+type McapFile struct {
+	Name string `json:"name"`
+	Size int64  `json:"size"`
+}
+
+type McapError struct {
+	Err     string `json:"error"`
+	Message string `json:"message"`
+}
 
 func main() {
 	r := chi.NewRouter()     // create a chi router
@@ -16,9 +32,11 @@ func main() {
 	r.Post("/upload", uploadHandler)
 	http.ListenAndServe(":3000", r) // start up server at :3000
 }
+
 func rootHandler(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte("Hello World"))
 }
+
 func uploadHandler(w http.ResponseWriter, r *http.Request) {
 	err := r.ParseMultipartForm(10 << 20)
 	if err != nil {
@@ -26,34 +44,39 @@ func uploadHandler(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	file, handler, err := r.FormFile("file")
+	file, header, err := r.FormFile("file")
 	if err != nil {
 		http.Error(w, "Unable to get file", http.StatusBadRequest)
 		return
 	}
 	defer file.Close()
 
-	filename := handler.Filename
+	filename := header.Filename
 	if !strings.HasSuffix(strings.ToLower(filename), ".mcap") {
 		w.Header().Set("Content-Type", "application/json")
-		w.WriteHeader(http.StatusUnsupportedMediaType) 
+		w.WriteHeader(http.StatusUnsupportedMediaType)
 
-		w.Write([]byte(`{
-    "error": "Unsupported file type",
-    "message": "The uploaded file type is not allowed. Please upload a .mcap file."
-}`))
+		errorResp := McapError{
+			Err:     "Unsupported file type",
+			Message: "The uploaded file type is not allowed. Please upload a .mcap file.",
+		}
+
+		json.NewEncoder(w).Encode(errorResp)
 		return
 	}
+
 	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(http.StatusOK)
 
-	jsonResponse := fmt.Sprintf(`{
-    "message": "MCAP uploaded successfully",
-    "file": {
-        "name": "%s",
-        "size": %d
-    }
-}`, filename, handler.Size)
+	fileInfo := McapFile{
+		Name: filename,
+		Size: header.Size,
+	}
 
-	w.Write([]byte(jsonResponse))
+	successResp := McapSuccess{
+		Message: "MCAP uploaded successfully",
+		File:    fileInfo,
+	}
+
+	json.NewEncoder(w).Encode(successResp)
 }
